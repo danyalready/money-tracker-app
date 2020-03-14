7fbcaed48b1cc1a6a744a6c821b2daae6ca3b4fc
ODanyor <dubashit@bk.ru>
2020-03-14 16:58:32 -0400

    Charts container

---
+++ src/containers/ChartsContainer/Styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ChartsContainer = styled.div`
+  border-left: 1px solid #aaa;
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 50% 50%;
+`;
---
+++ src/containers/ChartsContainer/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ChartsContainer } from "./Styles";
+
+const index = ({ children }) => {
+  return <ChartsContainer>{children}</ChartsContainer>;
+};
+
+export default index;
