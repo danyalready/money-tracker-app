2bc3f266f21e83949df37b49bd01409fe834420d
ODanyor <dubashit@bk.ru>
2020-03-14 12:29:27 -0400

    Forms porivided

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import MainContainer from "../../containers/MainContainer/index";
+// Redux
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addTransaction } from "../../store/actions/transactionActions";
 // Components
 import History from "../History/index";
 import Form from "../Form/index";
 
-const index = () => {
+const index = ({ addTransaction, loading, transactions }) => {
   return (
     <MainContainer>
-      <History />
-      <Form />
+      <History transactions={transactions} />
+      <Form addTransaction={addTransaction} />
       <h1>Charts</h1>
     </MainContainer>
   );
 };
 
-export default index;
+index.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  transactions: PropTypes.array.isRequired,
+  addTransaction: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = dispatch => ({
+  addTransaction: transaction => dispatch(addTransaction(transaction))
+});
+
+const mapStateToProps = state => ({
+  loading: state.transactions.loading,
+  transactions: state.transactions.transactions
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(index);
